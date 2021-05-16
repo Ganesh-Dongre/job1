f7f54d31c62246bb7082ce1b0afc1718936a46e9
Ganesh <g.dongre01234@gmail.com>
2021-05-16 18:01:30 +0530

    Committed for Poll SCM

--- Job.java
+++ Job.java
@@ -2,7 +2,7 @@ class Job
 { 
 public static void main(String args[])
 {
-	System.out.println("Job1 done again trying out pipeline");
+	System.out.println("Job1 done again trying Poll SCM Build");
 	System.out.println("Made some changes");
 }
 }
