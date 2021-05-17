27e36ae935c74b6b456d827f0dc53e62254a2436
Ganesh <g.dongre01234@gmail.com>
2021-05-17 12:34:32 +0530

    Version 2

--- Job.java
+++ Job.java
@@ -2,7 +2,7 @@ class Job
 { 
 public static void main(String args[])
 {
-	System.out.println("Job1 done rolled out V1.0");
+	System.out.println("Job1 done rolled out V2.0");
 	System.out.println("Made some changes");
 }
 }
